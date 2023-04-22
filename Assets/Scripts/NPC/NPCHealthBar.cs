548e977f44649d50ef6bb1cd68191b518f11c2cb
unknown <brendenkcheng@gmail.com>
2023-04-22 01:24:36 -0600

    Final

--- Assets/Scripts/NPC/NPCHealthBar.cs
+++ Assets/Scripts/NPC/NPCHealthBar.cs
@@ -7,28 +7,42 @@ namespace NPC
 {
     public class NPCHealthBar : MonoBehaviour
     {
-        private float health; 
-        private GameObject healthBar; //maybe make the health bar scale based on health
-        private Slider bar;
         private Transform playerPosition;  
+        private Quaternion initialRotation; 
+        private Animator textAnimator; 
+        private Animator imageAnimator;
 
         void Start()
         {
-            playerPosition = GameObject.FindWithTag("Player").transform;
-            healthBar = transform.GetChild(0).gameObject;
-            bar = transform.GetChild(0).GetChild(0).gameObject.GetComponent<Slider>(); 
-            bar.maxValue = gameObject.GetComponent<NonPlayerCharacter>().MaxHealth;
+            playerPosition = GameObject.FindWithTag("Player").transform.GetChild(1).transform;
+            textAnimator = this.gameObject.transform.GetChild(0).GetComponent<Animator>();
+            imageAnimator = this.gameObject.transform.GetChild(1).GetComponent<Animator>();
+        }
+
+        void Awake()
+        {
+            initialRotation = Quaternion.Euler(0, this.transform.localEulerAngles.y, 0);
         }
 
         void Update()
         {
-            Vector3 lookPosition = playerPosition.position - transform.position; 
-            lookPosition.y = 0; 
-            transform.rotation = Quaternion.LookRotation(lookPosition.normalized);
-            //healthBar.transform.LookAt(playerPosition);
-            health = gameObject.GetComponent<NonPlayerCharacter>().Health;
-            bar.value = health;
-        }
+            if((playerPosition.position - transform.position).magnitude < 6 && textAnimator.GetBool("Active") == false) 
+            {
+                Vector3 lookPosition = playerPosition.position - transform.position; 
+                lookPosition.y = 0;
+                transform.rotation = Quaternion.RotateTowards(Quaternion.LookRotation(transform.forward), Quaternion.LookRotation(-lookPosition.normalized), 100f * Time.deltaTime);
+            }
+            else if((playerPosition.position - transform.position).magnitude > 6)
+            {
+                transform.rotation = Quaternion.RotateTowards(Quaternion.LookRotation(transform.forward), initialRotation, 100f * Time.deltaTime);
+                textAnimator.SetBool("Active", false);
+                imageAnimator.SetBool("Active", true);
+            }
+            else if(transform.rotation.y != initialRotation.y && textAnimator.GetBool("Active") == false)
+            {
+                transform.rotation = Quaternion.RotateTowards(Quaternion.LookRotation(transform.forward), initialRotation, 100f * Time.deltaTime);
+            }
+        }   
 
     }
 }
